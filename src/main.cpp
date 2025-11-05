72706878fd52babc4cd1be18d148afade2659106
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2025-11-05 11:25:57 +0700

    remove clutter & vcpkg

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,8 @@
 #include <print>
-#include "../include/mqtt.h"
-#include "../include/ports.h"
-#include "../include/calculator.h"
-
-#ifdef WINDOWS_WINRT_ENABLED
-#include "../include/ble.h"
-#endif
-
-int run_calc(int agc, char **argv);
 
 int main(int argc, char* argv[])
 {
-    run_calc(argc, argv);
-    run_mqtt();
-
-#ifdef _WIN32
-    com_ports::run_com_ports();
-#endif
-
-#ifdef WINDOWS_WINRT_ENABLED
-    run_ble_scan();
-#endif
+	std::println("Hello, World!");
 
     return 0;
 }
